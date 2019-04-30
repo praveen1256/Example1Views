4f8fa3aaf096bfb9c319b6f2a7185707f0075242
Praveen M <mpraveen@jazeerapaints.com-at-238527534465>
2019-04-30 08:18:13 +0530

    Added Lifecycle Methods

--- app/src/main/java/android/com/example1views/MainActivity.java
+++ app/src/main/java/android/com/example1views/MainActivity.java
@@ -2,12 +2,58 @@ package android.com.example1views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MainActivity LifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(TAG,TAG+" : onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG,TAG+" : onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG,TAG+" : onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG,TAG+" : onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG,TAG+": onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG,TAG+" : onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(TAG,TAG+" : onRestart");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Log.v(TAG,TAG+" : onBackPressed");
     }
 }
