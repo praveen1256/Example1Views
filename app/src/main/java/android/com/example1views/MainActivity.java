b7c4eb6e2971479b432455eb3cf7415af04fe0da
Praveen M <mpraveen@jazeerapaints.com-at-238527534465>
2019-05-10 08:26:10 +0530

    Example ListView

--- app/src/main/java/android/com/example1views/MainActivity.java
+++ app/src/main/java/android/com/example1views/MainActivity.java
@@ -1,18 +1,47 @@
 package android.com.example1views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "MainActivity LifeCycle";
-
+    ListView lv_list;
+    String listData[] = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"};
+    int listImages[] = {R.drawable.ic_image1,R.drawable.ic_image2,R.drawable.ic_image1,R.drawable.ic_image2,R.drawable.ic_image1,
+            R.drawable.ic_image2,R.drawable.ic_image1,R.drawable.ic_image2,R.drawable.ic_image1,R.drawable.ic_image2,R.drawable.ic_image1,
+            R.drawable.ic_image2,R.drawable.ic_image1,R.drawable.ic_image2,R.drawable.ic_image1,R.drawable.ic_image2};
+    ArrayAdapter<String> stringArrayAdapter;
+    MyAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv_list = findViewById(R.id.lv_list);
+        setListView();
         Log.v(TAG,TAG+" : onCreate");
+    }
+
+    private void setListView() {
+//        stringArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1,listData);
+        stringArrayAdapter = new ArrayAdapter<String>(this, R.layout.item_list,R.id.tv_item_lv,listData);
+
+        // Custom Adapter
+        myAdapter = new MyAdapter(this,listData,listImages);
+
+        lv_list.setAdapter(myAdapter);
+        lv_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedString = listData[position];
+                Log.v(TAG,TAG+" : "+selectedString);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/android/com/example1views/MyAdapter.java
@@ -0,0 +1,37 @@
+package android.com.example1views;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyAdapter extends ArrayAdapter<String> {
+
+    String data[];
+    int images[];
+    Activity context;
+
+    public MyAdapter(Activity context,String data[],int images[]){
+        super(context,R.layout.item_list,data);
+        this.context = context;
+        this.data = data;
+        this.images = images;
+    }
+
+    @Override
+    public View getView(int position, View convertView,ViewGroup parent) {
+        LayoutInflater layoutInflater = context.getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.item_list,null,true);
+        ImageView imageView = view.findViewById(R.id.iv_icon);
+        TextView textView = view.findViewById(R.id.tv_item_lv);
+        imageView.setImageResource(images[position]);
+        textView.setText(data[position]);
+        Log.v("MyAdapter","Hai "+position+",Hashcode "+view.hashCode());
+        return view;
+    }
+}
