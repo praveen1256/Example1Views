b04531638107e27b654b85b3920b40e1e55648ef
Praveen M <mpraveen@jazeerapaints.com-at-238527534465>
2019-05-17 07:04:21 +0530

    SP Done

--- app/src/main/java/android/com/example1views/MainActivity.java
+++ app/src/main/java/android/com/example1views/MainActivity.java
@@ -1,17 +1,25 @@
 package android.com.example1views;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "MainActivity LifeCycle";
+    String MyPREFERENCES = "my_preference";
+
+    SharedPreferences sharedpreferences ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedpreferences = this.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         Log.v(TAG,TAG+" : onCreate");
     }
 
@@ -25,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.v(TAG,TAG+" : onResume");
+        getString();
     }
 
     @Override
@@ -56,4 +65,16 @@ public class MainActivity extends AppCompatActivity {
         super.onBackPressed();
         Log.v(TAG,TAG+" : onBackPressed");
     }
+
+    private void saveString(){
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString("place", "Hyderabad");
+        editor.commit();
+    }
+
+    private void getString(){
+        String place = sharedpreferences.getString("place","EMPTY");
+        Toast.makeText(this,place,Toast.LENGTH_LONG).show();
+    }
+
 }
