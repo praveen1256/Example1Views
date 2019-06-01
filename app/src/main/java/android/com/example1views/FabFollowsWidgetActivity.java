f8a1113d738199054d26c455514e4d8779b315d1
Praveen M <mpraveen@jazeerapaints.com-at-238527534465>
2019-06-01 07:08:34 +0530

    coordinator material design

---
+++ app/src/main/java/android/com/example1views/FabFollowsWidgetActivity.java
@@ -0,0 +1,14 @@
+package android.com.example1views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FabFollowsWidgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab_follows_widget);
+    }
+}
+
