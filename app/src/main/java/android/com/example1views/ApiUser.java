e072048423182a38cbc781999936b47692dfc184
Praveen M <mpraveen@jazeerapaints.com-at-238527534465>
2019-06-06 08:33:40 +0530

    rx java , map,filter

---
+++ app/src/main/java/android/com/example1views/ApiUser.java
@@ -0,0 +1,18 @@
+package android.com.example1views;
+
+public class ApiUser {
+
+    public long id;
+    public String firstname;
+    public String lastname;
+
+    @Override
+    public String toString() {
+        return "ApiUser{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+}
--- app/src/main/java/android/com/example1views/MainActivity.java
+++ app/src/main/java/android/com/example1views/MainActivity.java
@@ -3,15 +3,59 @@ package android.com.example1views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //https://www.androidhive.info/RxJava/android-rxjava-networking-with-retrofit-gson-notes-app/
+    //https://www.youtube.com/watch?v=Ocqj4vyzeQQ
+    //https://www.youtube.com/watch?v=mcVXDXt1s3E
+    //https://www.youtube.com/watch?v=ajOwLRLqaJ8
+    //https://blog.mindorks.com/understanding-rxjava-subject-publish-replay-behavior-and-async-subject-224d663d452f
+    //https://www.journaldev.com/20292/android-mvvm-design-pattern
+    //https://www.raywenderlich.com/8984-mvvm-on-android
+
+    //Rxjava
+    // We user multi threading.
+    // UI thread/Main Thread. Worker Thread or IO
+    // Observer Design Pattern
+        // Subject, Observable  , Observer , Subscribe
+        // ************************************************
+    //    Subject provides interface for observers to register and unregister themselves with the subject.
+    //    Subject knows who its subscribers are.
+    //    Multiple observers can subscribe for notifications.
+    //    Subject publishes the notifications.
+    //    Subject just sends the notification saying the state has changed. It does not pass any state information.
+    //    Once the notification is received from subject, observers call the subject and get data that is changed.
+
+    // Lamda
+    //  savedInstanceState->do work i.e savedInstanceState.get();
+
+
     String TAG = "MainActivity LifeCycle";
+    TextView textView;
+    public static final String LINE_SEPARATOR = "\n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_text);
         Log.v(TAG,TAG+" : onCreate");
     }
 
@@ -24,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        map();
+//        filter();
         Log.v(TAG,TAG+" : onResume");
     }
 
@@ -56,4 +102,155 @@ public class MainActivity extends AppCompatActivity {
         super.onBackPressed();
         Log.v(TAG,TAG+" : onBackPressed");
     }
+
+    private void map(){
+        getObservable()
+                // Run on a background thread
+                .subscribeOn(Schedulers.io())
+                // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Function<List<ApiUser>, List<User>>() {
+
+                    @Override
+                    public List<User> apply(List<ApiUser> apiUsers) {
+                        return convertApiUserListToUserList(apiUsers);
+                    }
+                })
+                .subscribe(getObserver());
+    }
+
+
+    private void filter(){
+        Observable.just(1, 2, 3, 4, 5, 6)
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) {
+                        String threadName = Thread.currentThread().getName();
+                        Toast.makeText(MainActivity.this,threadName,Toast.LENGTH_LONG).show();
+                        return integer % 2 == 0;
+                    }
+                })
+                .subscribe(getObserver2());
+    }
+
+    public static List<ApiUser> getApiUserList() {
+
+        List<ApiUser> apiUserList = new ArrayList<>();
+
+        ApiUser apiUserOne = new ApiUser();
+        apiUserOne.firstname = "Amit";
+        apiUserOne.lastname = "Shekhar";
+        apiUserList.add(apiUserOne);
+
+        ApiUser apiUserTwo = new ApiUser();
+        apiUserTwo.firstname = "Manish";
+        apiUserTwo.lastname = "Kumar";
+        apiUserList.add(apiUserTwo);
+
+        ApiUser apiUserThree = new ApiUser();
+        apiUserThree.firstname = "Sumit";
+        apiUserThree.lastname = "Kumar";
+        apiUserList.add(apiUserThree);
+
+        return apiUserList;
+    }
+
+    private Observable<List<ApiUser>> getObservable() {
+        return Observable.create(new ObservableOnSubscribe<List<ApiUser>>() {
+            @Override
+            public void subscribe(ObservableEmitter<List<ApiUser>> e) {
+                if (!e.isDisposed()) {
+                    String threadName = Thread.currentThread().getName();
+//                    Toast.makeText(MainActivity.this,threadName,Toast.LENGTH_LONG).show();
+                    Log.v("Log Name","Name : "+threadName);
+                    e.onNext(getApiUserList());
+                    e.onComplete();
+                }
+            }
+        });
+    }
+
+    public static List<User> convertApiUserListToUserList(List<ApiUser> apiUserList) {
+
+        List<User> userList = new ArrayList<>();
+
+        for (ApiUser apiUser : apiUserList) {
+            User user = new User();
+            user.firstname = apiUser.firstname;
+            user.lastname = apiUser.lastname;
+            userList.add(user);
+        }
+
+        return userList;
+    }
+
+    private Observer<List<User>> getObserver() {
+        return new Observer<List<User>>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, " onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(List<User> userList) {
+                textView.append(" onNext");
+                textView.append(LINE_SEPARATOR);
+                for (User user : userList) {
+                    textView.append(" firstname : " + user.firstname);
+                    textView.append(LINE_SEPARATOR);
+                }
+                Log.d(TAG, " onNext : " + userList.size());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                textView.append(" onError : " + e.getMessage());
+                textView.append(LINE_SEPARATOR);
+                Log.d(TAG, " onError : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append(" onComplete");
+                textView.append(LINE_SEPARATOR);
+                Log.d(TAG, " onComplete");
+            }
+        };
+    }
+
+    private Observer<Integer> getObserver2() {
+        return new Observer<Integer>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, " onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                textView.append(" onNext : ");
+                textView.append(LINE_SEPARATOR);
+                textView.append(" value : " + value);
+                textView.append(LINE_SEPARATOR);
+                Log.d(TAG, " onNext ");
+                Log.d(TAG, " value : " + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                textView.append(" onError : " + e.getMessage());
+                textView.append(LINE_SEPARATOR);
+                Log.d(TAG, " onError : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append(" onComplete");
+                textView.append(LINE_SEPARATOR);
+                Log.d(TAG, " onComplete");
+            }
+        };
+    }
+
 }
