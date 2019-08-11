02613e1a5ae301914052a213abc77577f31ccfc7
Praveen1756 <praveen1256.kumar@gmail.com>
2019-08-11 08:57:57 +0530

    small changes

--- app/src/main/java/android/com/example1views/MainActivity.java
+++ app/src/main/java/android/com/example1views/MainActivity.java
@@ -1,16 +1,15 @@
 package android.com.example1views;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Toast;
 
 import java.util.ArrayList;
